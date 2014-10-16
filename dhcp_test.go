317edbb91aef2b413785ff626b86c9c21d0c6a66
Brian Akins <bakins@omniti.com>
2014-10-16 17:27:29 -0400

    use fake http server for testing

--- dhcp_test.go
+++ dhcp_test.go
@@ -5,16 +5,19 @@ import (
 	"code.google.com/p/go.net/ipv4"
 	"crypto/rand"
 	"encoding/json"
+	"fmt"
 	dhcp "github.com/krolaw/dhcp4"
 	"github.com/mistifyio/mistify-agent/rpc"
 	"net"
 	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"reflect"
+	"runtime"
 	"testing"
 )
 
 func TestDHCP(t *testing.T) {
-	agent := "http://10.233.22.100:8080"
-	ifname := "eth1"
 
 	guest := rpc.Guest{
 		Memory: 512,
@@ -25,6 +28,7 @@ func TestDHCP(t *testing.T) {
 				Address: "10.0.0.2",
 				Netmask: "255.255.255.0",
 				Gateway: "10.0.0.1",
+				Mac:     "DE:AD:BE:EF:3D:F4",
 			},
 		},
 		Disks: []rpc.Disk{
@@ -33,49 +37,43 @@ func TestDHCP(t *testing.T) {
 	}
 
 	guestJSON, err := json.Marshal(guest)
-	if err != nil {
-		t.Fatalf("Couldn't decode JSON: %s\n", err.Error())
-	}
+	ok(t, err)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(guestJSON)
+	}))
+	defer ts.Close()
+
+	ifname := "eth1"
 
 	// Create UDP connection
 	udpconn, err := net.ListenPacket("udp4", net.JoinHostPort(net.IPv4zero.String(), "68"))
-	if err != nil {
-		t.Fatalf("ListenPacket failed: %s\n", err.Error())
-	}
+	ok(t, err)
 
 	// Create PacketConn with UDP connection as its transport
 	pconn := ipv4.NewPacketConn(udpconn)
 
 	// Get interface index and create a control message
 	iface, err := net.InterfaceByName(ifname)
-	if err != nil {
-		t.Fatalf("Can't get interface: %s\n", err.Error())
-	}
+	ok(t, err)
 
 	cm := ipv4.ControlMessage{IfIndex: iface.Index}
 
 	// Create a new guest
-	resp, err := http.Post(agent+"/guests", "application/json", bytes.NewReader(guestJSON))
-	if err != nil {
-		t.Fatalf("Couldn't POST: %s\n", err.Error())
-	}
+	resp, err := http.Post(ts.URL+"/guests", "application/json", bytes.NewReader(guestJSON))
+	ok(t, err)
 
 	defer resp.Body.Close()
 
-	// Read the response into the "guest" object created earlier
-	buf := new(bytes.Buffer)
-	if _, err = buf.ReadFrom(resp.Body); err != nil {
-		t.Fatalf("Can't read response: %s\n", err.Error())
-	}
+	err = json.NewDecoder(resp.Body).Decode(&guest)
+	ok(t, err)
 
-	json.Unmarshal(buf.Bytes(), &guest)
-
+	fmt.Printf("Guest: %+v\n", guest)
 	// Just need any NIC
 	nic := guest.Nics[0]
+	fmt.Println(nic.Mac)
 	mac, err := net.ParseMAC(nic.Mac)
-	if err != nil {
-		t.Fatalf("Can't parse MAC: %s\n", err.Error())
-	}
+	ok(t, err)
 
 	// Generate xid for discover packet - 4 random bytes
 	xid := make([]byte, 4)
@@ -85,9 +83,7 @@ func TestDHCP(t *testing.T) {
 	packet := dhcp.RequestPacket(dhcp.Discover, mac, net.IPv4zero, xid, true, []dhcp.Option{})
 
 	_, err = pconn.WriteTo(packet, &cm, &net.UDPAddr{IP: net.IPv4bcast, Port: 67})
-	if err != nil {
-		t.Fatalf("Can't write packet: %s\n", err.Error())
-	}
+	ok(t, err)
 
 	// Listen for a response
 	response := make(dhcp.Packet, 512)
@@ -95,32 +91,51 @@ func TestDHCP(t *testing.T) {
 
 	// Make sure it's an offer and the IP address in the response packet matches our guest's
 	mtype := dhcp.MessageType(response.ParseOptions()[dhcp.OptionDHCPMessageType][0])
-	if mtype != dhcp.Offer {
-		t.Fatalf("Response was not an offer\n")
-	}
+	equals(t, dhcp.Offer, mtype)
 
-	if !response.YIAddr().Equal(net.ParseIP(nic.Address)) {
-		t.Fatalf("Got wrong IP address (%v != %v)\n", response.YIAddr(), nic.Address)
-	}
+	assert(t, response.YIAddr().Equal(net.ParseIP(nic.Address)), "Got wrong IP address: %v != %v", response.YIAddr(), nic.Address)
 
 	// Send back a request packet
 	packet = dhcp.RequestPacket(dhcp.Request, mac, response.YIAddr(), xid, true, []dhcp.Option{})
 
 	_, err = pconn.WriteTo(packet, &cm, &net.UDPAddr{IP: net.IPv4bcast, Port: 67})
-	if err != nil {
-		t.Fatalf("Can't write packet: %s\n", err.Error())
-	}
+	ok(t, err)
 
 	// Listen for a response
 	pconn.ReadFrom(response)
 
 	// Make sure it's an ack and the IP address in the response packet matches our guest's
 	mtype = dhcp.MessageType(response.ParseOptions()[dhcp.OptionDHCPMessageType][0])
-	if mtype != dhcp.ACK {
-		t.Fatalf("Response was not an ACK\n")
-	}
+	equals(t, dhcp.ACK, mtype)
 
-	if !response.YIAddr().Equal(net.ParseIP(nic.Address)) {
-		t.Fatalf("Got wrong IP address (%v != %v)\n", response.YIAddr(), nic.Address)
+	assert(t, response.YIAddr().Equal(net.ParseIP(nic.Address)), "Got wrong IP address: %v != %v", response.YIAddr(), nic.Address)
+}
+
+// https://github.com/benbjohnson/testing
+
+// assert fails the test if the condition is false.
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// ok fails the test if an err is not nil.
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
 	}
 }
