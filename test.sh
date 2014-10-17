fc1160bafb49cab089fe792a9d4222235d00f9f3
Brian Akins <bakins@omniti.com>
2014-10-17 15:07:22 -0400

    add ip check

--- test.sh
+++ test.sh
@@ -20,6 +20,8 @@ sleep 1
 
 IP=$(sudo dhcping -h de:ad:be:ef:3d:f4 -s 127.0.0.1 -V | awk '/^yiaddr/ {print $NF}' | sort | tail -n 1)
 
+echo Got IP $IP
+
 if [ "$IP" != "10.0.0.2" ]; then
     echo "did not get expect ip"
 fi
